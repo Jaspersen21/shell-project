ba69313e4443dfced6ebbe43b48c2fd6ed4c0521
Jaspersen21 <digo@digo.digo>
2024-12-04 12:25:54 +0300

    [exit command]

--- app/main.py
+++ app/main.py
@@ -15,11 +15,8 @@ def main():
             # Split the command to check for an exit code
             parts = command.split()
             if len(parts) == 2 and parts[1].isdigit():  # Ensure the exit code is a number
-                exit_code = int(parts[1])  # Convert the exit code to an integer
-                print(f"Exiting with code {exit_code}")
-                sys.exit(exit_code)  # Exit with the specified code
+                sys.exit(int(parts[1]))  # Exit with the specified code
             elif len(parts) == 1:
-                print("Exiting with code 0")
                 sys.exit(0)  # Default exit code is 0
             else:
                 print(f"{command}: invalid syntax")
